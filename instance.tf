8daa205b55d037620751f07c02dddc8399f63f68
enver0173 <--list>
2020-01-26 16:35:02 -0600

    sddd

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami               = "${data.aws_ami.centos.id}"
   instance_type     = "${var.instance_type}"
   key_name          = "${aws_key_pair.bastion-wordpress.key_name}"
-  vpc_security_group_ids = "${[aws_security_group.ssh.id]}"
+  vpc_security_group_ids = "${aws_security_group.ssh.id}"
   
   provisioner "remote-exec" {
     connection {
